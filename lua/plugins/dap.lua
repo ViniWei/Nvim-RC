f81096692ffe2ccf6a639a38c3db1f69699a5389
unknown <viniciusweigert152@gmail.com>
2023-12-05 15:53:43 -0300

    Dap configuration

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,19 +1,38 @@
 local dap = require('dap')
 vim.keymap.set('n', '<leader>d', dap.toggle_breakpoint, {})
 
--- dap.adapters.coreclr = {
---   type = 'executable',
---   command = '/path/to/dotnet/netcoredbg/netcoredbg',
---   args = {'--interpreter=vscode'}
--- }
--- 
--- dap.configurations.cs = {
---   {
---     type = "coreclr",
---     name = "launch - netcoredbg",
---     request = "launch",
---     program = function()
---         return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
---     end,
---   },
--- }
+-- c#
+dap.adapters.coreclr = {
+  type = 'executable',
+  command = 'C:/Users/Vinicius/netcoredbg/netcoredbg',
+  args = {'--interpreter=vscode'}
+}
+
+dap.configurations.cs = {
+  {
+    type = "coreclr",
+    name = "launch - netcoredbg",
+    request = "launch",
+    program = function()
+        return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+    end,
+  },
+}
+
+dap.adapters.gdb = {
+  type = "executable",
+  command = "gdb",
+  args = { "-i", "dap" }
+}
+
+dap.configurations.c = {
+  {
+    name = "Launch",
+    type = "gdb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = "${workspaceFolder}",
+  },
+}
