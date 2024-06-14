0de86cf5f00081e09460cfd2dace59215e412c80
Vinícius Weigert <viniciusweigert152@gmail.com>
2024-06-14 17:53:52 -0300

    LSP Mason Cmp

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -1,4 +1,12 @@
-vim.keymap.set("n", "<leader>w", ":w<enter>")  
+vim.keymap.set("n", "<leader>w", ":w<enter>")
 
-vim.keymap.set("n", "<leader>o", "o<esc>")  
-vim.keymap.set("n", "<leader>O", "O<esc>")  
+vim.keymap.set("n", "<leader>o", "o<esc>")
+vim.keymap.set("n", "<leader>O", "O<esc>")
+
+vim.keymap.set("n", "<leader>h", ":noh<enter>")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,6 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
+vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
@@ -13,14 +13,21 @@ vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
     {
-        "greflm13/monokai_vibrant",
-        config = function()
-             vim.cmd.colorscheme("monokai_vibrant")
+        "loctvl842/monokai-pro.nvim",
+        config = function ()
+            require("monokai-pro").setup({
+                override = function()
+                    return {
+                        Normal = { bg = "#0e0e12" }
+                    }
+                end
+            })
+            vim.cmd("colorscheme monokai-pro-spectrum")
         end
     },
     {
         "nvim-treesitter/nvim-treesitter",
-        config = function() 
+        config = function()
            require("nvim-treesitter.configs").setup({
                 ensure_installed = {"c", "lua", "vim", "vimdoc", "query" },
 
@@ -30,6 +37,89 @@ local plugins = {
                     enable = true
                 }
            })
+        end
+    },
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "mason.nvim" },
+        config = function()
+            local mason_lspconfig = require("mason-lspconfig")
+
+            mason_lspconfig.setup()
+            mason_lspconfig.setup_handlers({
+                function (server_name)
+                    require("lspconfig")[server_name].setup({})
+                end,
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function ()
+            vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+            vim.api.nvim_set_keymap("n", "<leader>k", "<cmd>lua vim.lsp.buf.hover()<CR>", {noremap = true})
+            vim.api.nvim_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", {noremap = true})
+        end
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        config = function()
+            local cmp = require('cmp')
+            cmp.setup({
+                sources = {
+                    { name = 'nvim_lsp' }
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                    ['<C-d>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior, count = 1 }),
+                    ['<C-u>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior, count = 1 })
+                }),
+            })
+        end
+    },
+    {
+        "sar/cmp-lsp.nvim",
+        dependencies = { "nvim-lspconfig" },
+        config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            require('lspconfig').lua_ls.setup({ capabilities = capabilities })
+        end
+    },
+    {
+        "m4xshen/autoclose.nvim",
+        config = function()
+            require("autoclose").setup()
+        end
+    },
+    {
+        'nvim-lua/plenary.nvim'
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function ()
+            require('telescope').setup({
+                defaults = {
+                    file_ignore_patterns = {
+                        "node_modules"
+                    }
+                }
+            })
+
+            vim.keymap.set("n", "<C-s>", ":Telescope find_files<enter>")
+        end
+    },
+    {
+        "ThePrimeagen/harpoon",
+        config = function ()
+            vim.keymap.set("n", "<leader>q", ":lua require('harpoon.cmd-ui').toggle_quick_menu()<enter>")
+            vim.keymap.set("n", "<leader>aq", ":lua require('harpoon.mark').add_file()<enter>")
         end
     }
 }
