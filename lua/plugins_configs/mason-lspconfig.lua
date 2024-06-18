9a696b9bdef804b548eb9a91ef7bcf0da3ff4057
Vinícius Weigert <vweigert@gocn.com.br>
2024-06-18 09:27:22 -0300

    Ensure installed lua_ls

--- lua/plugins_configs/mason-lspconfig.lua
+++ lua/plugins_configs/mason-lspconfig.lua
@@ -4,7 +4,10 @@ return {
         config = function()
             local mason_lspconfig = require("mason-lspconfig")
 
-            mason_lspconfig.setup()
+            mason_lspconfig.setup({
+                ensure_installed = { "lua_ls" }
+            })
+
             mason_lspconfig.setup_handlers({
                 function (server_name)
                     require("lspconfig")[server_name].setup({})
