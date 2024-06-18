a54cbfd4c813eb00106a018a27ed7e959234d986
Vinícius Weigert <vweigert@gocn.com.br>
2024-06-18 09:43:55 -0300

    Change harpoon and close keybinds

--- lua/plugins_configs/harpoon.lua
+++ lua/plugins_configs/harpoon.lua
@@ -3,8 +3,8 @@ return {
     config = function ()
         require("harpoon").setup({})
 
-        vim.keymap.set('n', '<leader>q', function() require('harpoon.ui').toggle_quick_menu() end, {noremap = true})
-        vim.keymap.set('n', '<leader>aq', function() require("harpoon.mark").add_file() end, {noremap = true})
+        vim.keymap.set('n', '<leader>h', function() require('harpoon.ui').toggle_quick_menu() end, {noremap = true})
+        vim.keymap.set('n', '<leader>ah', function() require("harpoon.mark").add_file() end, {noremap = true})
 
         vim.keymap.set('n', '<leader>1', function() require("harpoon.ui").nav_file(1) end, {noremap = true})
         vim.keymap.set('n', '<leader>2', function() require("harpoon.ui").nav_file(2) end, {noremap = true})
