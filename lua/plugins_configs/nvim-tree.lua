a6598b498e09a742ea3f3efe0895fd338ec1afc7
Vinícius Weigert <vweigert@gocn.com.br>
2024-06-18 15:08:07 -0300

    Update tree on open file

--- lua/plugins_configs/nvim-tree.lua
+++ lua/plugins_configs/nvim-tree.lua
@@ -33,6 +33,9 @@ return {
             view = {
                 side = "right",
             },
+            update_focused_file = {
+                enable = true
+            },
             on_attach = my_on_attach,
         })
 
