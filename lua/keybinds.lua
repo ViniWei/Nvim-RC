1e287ab75357f55bf125d25f54200444bf05f1d6
Vinícius Weigert <vweigert@gocn.com.br>
2024-06-19 11:23:36 -0300

    Change :noh keybind

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -3,7 +3,7 @@ vim.keymap.set("n", "<leader>w", ":w<enter>")
 vim.keymap.set("n", "<leader>o", "o<esc>")
 vim.keymap.set("n", "<leader>O", "O<esc>")
 
-vim.keymap.set("n", "<leader>h", ":noh<enter>")
+vim.keymap.set("n", "<leader>n", ":noh<enter>")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
