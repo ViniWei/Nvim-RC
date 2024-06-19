7f1e4b9eb915676e079803f6c7e677ec91ab2f98
Vinícius Weigert <viniciusweigert152@gmail.com>
2024-06-19 17:48:37 -0300

    Now using lsp-zero

--- lua/plugins_configs/cmp-lsp.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "sar/cmp-lsp.nvim",
-    dependencies = { "nvim-lspconfig" },
-    config = function()
-        local capabilities = require('cmp_nvim_lsp').default_capabilities()
-        require('lspconfig').lua_ls.setup({ capabilities = capabilities })
-    end
-}
---
+++ lua/plugins_configs/lsp-zero.lua
@@ -0,0 +1,56 @@
+return {
+    {
+        dependencies = { "williamboman/mason.nvim" },
+        "williamboman/mason-lspconfig.nvim"
+    },
+    {"neovim/nvim-lspconfig"},
+    {
+        "hrsh7th/cmp-nvim-lsp",
+        config = function()
+            local cmp = require('cmp')
+            cmp.setup({
+                sources = {
+                    { name = 'nvim_lsp' }
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                    ['<C-d>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior, count = 1 }),
+                    ['<C-u>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior, count = 1 })
+                }),
+            })
+        end
+    },
+    {"hrsh7th/nvim-cmp"},
+    {"L3MON4D3/LuaSnip"},
+
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v3.x",
+        dependencies = { "williamboman/mason-lspconfig.nvim" },
+        config = function ()
+            local lsp_zero = require("lsp-zero")
+
+            -- LSP keybinds -- 
+            lsp_zero.on_attach(function(client, bufnr)
+                lsp_zero.default_keymaps({buffer = bufnr})
+
+                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, { noremap = true, silent = true })
+                vim.keymap.set("n", "<leader>k", function() vim.lsp.buf.hover() end, { noremap = true })
+                vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, { noremap = true })
+                vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, { noremap = true })
+                vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, { noremap = true })
+            end)
+            --
+
+            require("mason-lspconfig").setup({
+                ensure_installed = {},
+                handlers = {
+                    function(server_name)
+                        require("lspconfig")[server_name].setup({})
+                    end,
+                },
+            })
+
+        end
+    },
+}
