6ba4440d385b52f683366a8ad8b189ad1b1a16b5
Vinícius Weigert <viniciusweigert152@gmail.com>
2024-06-20 19:13:50 -0300

    Remove Ctrl + c keybind on terminal mode

--- lua/plugins_configs/toggleterm.lua
+++ lua/plugins_configs/toggleterm.lua
@@ -12,7 +12,6 @@ return {
             vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
             vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
             vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-            vim.keymap.set('t', '<C-c>', [[<C-\><C-n><C-w>]], opts)
         end
         -- if you only want these mappings for toggle term use term://*toggleterm#* instead
         vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
