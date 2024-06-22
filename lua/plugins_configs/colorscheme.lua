d0e3fbb884f9e608c2b427274010f9bad56f4082
Vinícius Weigert <viniciusweigert152@gmail.com>
2024-06-22 18:07:04 -0300

    Changed colorscheme options

--- lua/plugins_configs/colorscheme.lua
+++ lua/plugins_configs/colorscheme.lua
@@ -2,9 +2,25 @@ return {
     "loctvl842/monokai-pro.nvim",
     config = function ()
         require("monokai-pro").setup({
+            styles = {
+                comment = { italic = false },
+                keyword = { italic = false },
+                type = { italic = false },
+                storageclass = { italic = false },
+                structure = { italic = false },
+                parameter = { italic = false },
+                annotation = { italic = false },
+                tag_attribute = { italic = false },
+            },
             override = function()
                 return {
-                    Normal = { bg = "#0e0e12" }
+                    Normal = { bg = "#0e0e12" },
+                }
+            end,
+            overridePalette = function(filter)
+                return {
+                    dark1 = "#09090c",
+                    background = "#0e0e12",
                 }
             end
         })
