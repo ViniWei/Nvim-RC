9314d64233669139c405671ce5bbc12d9eb2d60b
Vinícius Weigert <viniciusweigert152@gmail.com>
2024-09-14 00:18:48 -0300

    Add guess-indent

--- lua/auto_cmds.lua
+++ lua/auto_cmds.lua
@@ -3,6 +3,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Hightlight selection on yank',
   pattern = '*',
   callback = function()
-      vim.highlight.on_yank { higroup = 'IncSearch', timeout = 50 }
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 50 }
   end,
 })
