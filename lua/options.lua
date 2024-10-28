346e6e79cd9dc0f23543d4c10701b697b339c319
ViniWei <viniciusweigert152@gmail.com>
2024-10-27 23:01:26 -0300

    Netrw icons

--- lua/options.lua
+++ lua/options.lua
@@ -21,6 +21,9 @@ vim.opt.termguicolors = true
 
 vim.g.mapleader = " "
 
+vim.g.netrw_banner = 0
+vim.g.netrw_list_hide= ".*\\.swp$,.DS_Store,*/tmp/*,*.so,*.swp,*.git,*.zip,^\\.\\.\\=/\\=$"
+
 vim.opt.updatetime = 100
 
 vim.opt.timeoutlen = 800
