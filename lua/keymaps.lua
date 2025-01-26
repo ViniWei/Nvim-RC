c896bf8eb75c85dd66de4886b57344972b24786e
ViniWei <viniciusweigert152@gmail.com>
2025-01-25 23:55:06 -0300

    Clean config

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -13,9 +13,6 @@ vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 
-vim.keymap.set("n", "<leader>q", ":q<enter>", { silent = true })
-vim.keymap.set("n", "<leader>!", ":q!<enter>", { silent = true })
-
 vim.keymap.set("n", "<leader>f", ":tab split<enter>")
 
 vim.keymap.set("n", "<C-x>", ":cnext<enter>")
@@ -23,16 +20,3 @@ vim.keymap.set("n", "<C-z>", ":cprevious<enter>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
--- Netrw mappings --
-vim.keymap.set("n", "<C-e>", ":Ex<enter>")
-
-local function set_netrw_mappings()
-    vim.keymap.set("n", "s", "<enter>", { remap = true, buffer = true })
-    vim.keymap.set("n", "<C-e>", "-", { remap = true, buffer = true })
-end
-
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "netrw",
-    callback = set_netrw_mappings,
-})
--- lua/plugins_configs/colorscheme.lua
+++ lua/plugins_configs/colorscheme.lua
@@ -1,31 +1,4 @@
 return {
-    --"loctvl842/monokai-pro.nvim",
-    --config = function ()
-    --    require("monokai-pro").setup({
-    --        styles = {
-    --            comment = { italic = false },
-    --            keyword = { italic = false },
-    --            type = { italic = false },
-    --            storageclass = { italic = false },
-    --            structure = { italic = false },
-    --            parameter = { italic = false },
-    --            annotation = { italic = false },
-    --            tag_attribute = { italic = false },
-    --        },
-    --        override = function()
-    --            return {
-    --                Normal = { bg = "#0e0e12" },
-    --            }
-    --        end,
-    --        overridePalette = function(filter)
-    --            return {
-    --                dark1 = "#09090c",
-    --                background = "#0e0e12",
-    --            }
-    --        end
-    --    })
-    --    vim.cmd("colorscheme monokai-pro-spectrum")
-    --end
     {
         "rebelot/kanagawa.nvim",
         config = function ()
