40007be10fb863b26dd5fb9965050e1a5b5232f7
ViniWei <viniciusweigert152@gmail.com>
2025-02-01 16:42:01 -0300

    Make ts_ls work with vue

--- lua/plugins_configs/lsp.lua
+++ lua/plugins_configs/lsp.lua
@@ -10,6 +10,21 @@ local function lua_ls_setup()
     }
 end
 
+local function ts_ls_setup()
+    require("lspconfig").ts_ls.setup {
+        filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+        init_options = {
+            plugins = {
+                {
+                    name = '@vue/typescript-plugin',
+                    location = vim.fn.stdpath 'data' .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
+                    languages = { 'vue' },
+                },
+            },
+        },
+    }
+end
+
 return {
     {
         dependencies = { "williamboman/mason.nvim", "saghen/blink.cmp" },
@@ -18,7 +33,7 @@ return {
             local mason_lspconfig = require("mason-lspconfig")
 
             mason_lspconfig.setup({
-                ensure_installed = { "lua_ls", "jsonls", "ts_ls" }
+                ensure_installed = { "lua_ls", "jsonls", "ts_ls", "volar" }
             })
             mason_lspconfig.setup_handlers({
                 function(server_name)
@@ -26,6 +41,9 @@ return {
                 end,
                 ["lua_ls"] = function()
                     lua_ls_setup()
+                end,
+                ["ts_ls"] = function ()
+                    ts_ls_setup()
                 end
             })
         end
