dc748b02d0a2aad15eb18ecd96f09cb648ef2ec1
ViniWei <viniciusweigert152@gmail.com>
2025-02-01 01:12:27 -0300

    Remove dap and lualine project_name

--- lua/plugins_configs/lualine.lua
+++ lua/plugins_configs/lualine.lua
@@ -1,27 +1,3 @@
-local function split_string(inputstr, sep)
-  if sep == nil then
-    sep = "%s"
-  end
-  local t = {}
-  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-    table.insert(t, str)
-  end
-  return t
-end
-
-local function project_name()
-    local project_path = vim.inspect(vim.lsp.buf.list_workspace_folders()[1])
-
-    if (project_path ~= "nil") then
-        project_path = split_string(project_path, "/")
-        local project_folder = project_path[#project_path]
-        project_folder = project_folder:sub(1, -2)
-
-        return project_folder
-    end
-    return ""
-end
-
 return {
     "nvim-lualine/lualine.nvim",
     config = function ()
@@ -32,7 +8,7 @@ return {
                 theme = "auto",
             },
             sections = {
-                lualine_c = { { 'filename', path = 1 }, project_name,  },
+                lualine_c = { { 'filename', path = 1 } },
                 lualine_x = { 'encoding', 'fileformat', 'filetype' }
             }
         })
