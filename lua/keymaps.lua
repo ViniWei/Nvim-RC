197d4d2dd315d00992449d415176fbfa4a3b4aab
ViniWei <viniciusweigert152@gmail.com>
2025-02-01 16:11:53 -0300

    Add lsp keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -20,3 +20,8 @@ vim.keymap.set("n", "<C-z>", ":cprevious<enter>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- lsp keymaps (remove in the future)
+vim.keymap.set('n', 'grn', vim.lsp.buf.rename)
+vim.keymap.set('n', 'gra', vim.lsp.buf.code_action)
+vim.keymap.set('n', 'grr', vim.lsp.buf.references)
