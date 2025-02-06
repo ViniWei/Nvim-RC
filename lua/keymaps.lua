4dd6718ba8698539986b4027a1f0d000a33960b4
Vinícius Weigert <vweigert@gocn.com.br>
2025-02-06 11:29:50 -0300

    Fix format keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -22,7 +22,7 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
-vim.keymap.set('n', 'fm', vim.lsp.buf.format)
+vim.keymap.set('n', '<leader>fm', vim.lsp.buf.format)
 -- Future defaults
 vim.keymap.set('n', 'grn', vim.lsp.buf.rename)
 vim.keymap.set('n', 'gra', vim.lsp.buf.code_action)
