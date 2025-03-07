ae310ac11fa6f25188c3aaebe6cf2a0f5af328ee
Vinícius Weigert <vweigert@gocn.com.br>
2025-03-07 16:08:16 -0300

    Disable completion for shell commands

--- lua/plugins_configs/completion.lua
+++ lua/plugins_configs/completion.lua
@@ -8,6 +8,15 @@ return {
             nerd_font_variant = "mono"
         },
         signature = { enabled = true },
+        sources = {
+            providers = {
+                cmdline = {
+                    enabled = function()
+                        return not string.match(vim.fn.getcmdline(), "!")
+                    end
+                }
+            }
+        }
     },
     opts_extend = { "sources.default" }
 }
