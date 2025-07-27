970b559fe8850d84ca826a081e499f468d47323b
ViniWei <viniciusweigert152@gmail.com>
2025-07-27 13:41:36 -0300

    Better harpoon keymaps

--- lua/plugins_configs/harpoon.lua
+++ lua/plugins_configs/harpoon.lua
@@ -3,12 +3,13 @@ return {
     config = function ()
         require("harpoon").setup({})
 
-        vim.keymap.set('n', '<leader>h', function() require('harpoon.ui').toggle_quick_menu() end, {noremap = true})
-        vim.keymap.set('n', '<leader>ah', function() require("harpoon.mark").add_file() end, {noremap = true})
+        vim.keymap.set('n', '<leader>jm', function() require('harpoon.ui').toggle_quick_menu() end, {noremap = true})
+        vim.keymap.set('n', '<leader>ji', function() require("harpoon.mark").add_file() end, {noremap = true})
 
-        vim.keymap.set('n', '<leader>1', function() require("harpoon.ui").nav_file(1) end, {noremap = true})
-        vim.keymap.set('n', '<leader>2', function() require("harpoon.ui").nav_file(2) end, {noremap = true})
-        vim.keymap.set('n', '<leader>3', function() require("harpoon.ui").nav_file(3) end, {noremap = true})
-        vim.keymap.set('n', '<leader>4', function() require("harpoon.ui").nav_file(4) end, {noremap = true})
+        vim.keymap.set('n', '<leader>ja', function() require("harpoon.ui").nav_file(1) end, {noremap = true})
+        vim.keymap.set('n', '<leader>js', function() require("harpoon.ui").nav_file(2) end, {noremap = true})
+        vim.keymap.set('n', '<leader>jd', function() require("harpoon.ui").nav_file(3) end, {noremap = true})
+        vim.keymap.set('n', '<leader>jf', function() require("harpoon.ui").nav_file(4) end, {noremap = true})
+        vim.keymap.set('n', '<leader>jg', function() require("harpoon.ui").nav_file(5) end, {noremap = true})
     end
 }
