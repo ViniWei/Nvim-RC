3d872c50ecaa281b8929da9b8bec12edac6b7865
ViniWei <viniciusweigert152@gmail.com>
2025-08-05 23:31:52 -0300

    Add vue filetype to ts_ls config and enable vue_ls

--- lua/plugins_configs/lsp.lua
+++ lua/plugins_configs/lsp.lua
@@ -10,12 +10,20 @@ local function lua_ls_setup()
     })
 end
 
+local function ts_ls_setup()
+    vim.lsp.config("ts_ls", {
+        filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "vue" }
+    })
+end
+
 return {
     {
         "neovim/nvim-lspconfig",
         config = function ()
            lua_ls_setup()
-           vim.lsp.enable({ "ts_ls", "jsonls", "pylsp", "lua_ls" })
+           ts_ls_setup()
+
+           vim.lsp.enable({ "ts_ls", "jsonls", "pylsp", "lua_ls", "vue_ls" })
         end
     }
 }
