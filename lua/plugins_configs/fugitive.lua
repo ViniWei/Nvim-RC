1b31cf7e89968dfb5b2d0b79c57786afc79ce86d
Vinicius Weigert <vweigert@gocn.com.br>
2025-08-07 14:22:33 -0300

    Change fugitive keymap

--- lua/plugins_configs/fugitive.lua
+++ lua/plugins_configs/fugitive.lua
@@ -5,7 +5,7 @@ return {
     },
     "tpope/vim-fugitive",
     config = function ()
-       vim.keymap.set("n", "<leader>go", ":G<enter><C-w>o", { silent = true })
+       vim.keymap.set("n", "<leader>go", ":G<enter>", { silent = true })
        vim.keymap.set("n", "<leader>gu", ":GBrowse<enter>")
     end
 }
