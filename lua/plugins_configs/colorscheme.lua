8afee79101d2a0af374fcdbbd39a2e0912e62054
ViniWei <viniciusweigert152@gmail.com>
2025-08-10 01:37:45 -0300

    Change colorscheme to kanagawa

--- lua/plugins_configs/colorscheme.lua
+++ lua/plugins_configs/colorscheme.lua
@@ -1,13 +1,33 @@
 return {
-  "vague2k/vague.nvim",
-  config = function()
-    require("vague").setup({
-      transparent = true,
-      style = {
-        comments = "none",
-        strings = "none",
-      },
-    })
-    vim.cmd("colorscheme vague")
-  end
+  -- {
+  --   "vague2k/vague.nvim",
+  --   config = function()
+  --     require("vague").setup({
+  --       transparent = true,
+  --       style = {
+  --         comments = "none",
+  --         strings = "none",
+  --       },
+  --     })
+  --     vim.cmd("colorscheme vague")
+  --   end
+  -- },
+  {
+    "rebelot/kanagawa.nvim",
+    config = function ()
+      require('kanagawa').setup({
+        transparent = true,
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = "none"
+              }
+            }
+          }
+        }
+      })
+      vim.cmd("colorscheme kanagawa")
+    end
+  }
 }
