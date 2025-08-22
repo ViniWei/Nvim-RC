2484bb4bc407114b2f5d4d10b4c0e82fc77d3d57
ViniWei <viniciusweigert152@gmail.com>
2025-08-22 09:36:32 -0300

    Remove mini icons

--- lua/plugins_configs/oil.lua
+++ lua/plugins_configs/oil.lua
@@ -1,7 +1,6 @@
 return {
   "stevearc/oil.nvim",
   opts = {},
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
   config = function ()
     require("oil").setup({
       use_default_keymaps = false,
