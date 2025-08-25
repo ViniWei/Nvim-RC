e6f7410938de8c9ae2ae80c9c2bc7699a0e447a4
ViniWei <viniciusweigert152@gmail.com>
2025-08-25 20:08:20 -0300

    Use default keymaps for blink

--- lua/plugins_configs/completion.lua
+++ lua/plugins_configs/completion.lua
@@ -2,7 +2,6 @@ return {
     "saghen/blink.cmp",
     version = "*",
     opts = {
-        keymap = { preset = "enter" },
         appearance = {
             use_nvim_cmp_as_default = true,
             nerd_font_variant = "mono"
