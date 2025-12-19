7566c37e1459ec076b0c1d914e22307919c620fe
ViniWei <viniciusweigert152@gmail.com>
2025-12-19 17:04:06 -0300

    Make snippets autorefresh

--- lua/plugins_configs/luasnip.lua
+++ lua/plugins_configs/luasnip.lua
@@ -5,6 +5,10 @@ return {
         require("luasnip.loaders.from_snipmate").load()
         local ls = require("luasnip")
 
+        ls.config.set_config({
+            updateevents = "TextChanged,TextChangedI"
+        })
+
         vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
         vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
         vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
