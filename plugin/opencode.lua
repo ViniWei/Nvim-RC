2c71535df621a58deea6d6a4b4d3df941decf2e0
ViniWei <viniciusweigert152@gmail.com>
2026-04-26 17:19:33 -0300

    add opencode nvim extension

---
+++ plugin/opencode.lua
@@ -0,0 +1,6 @@
+vim.pack.add({"https://github.com/nickjvandyke/opencode.nvim"})
+
+vim.keymap.set("n", "<leader>ot", function() require("opencode").toggle() end, { desc = "Toggle opencode" })
+vim.keymap.set({ "n", "x" }, "<leader>oa", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
+vim.keymap.set({ "n", "x" }, "<leader>od", function() require("opencode").select() end, { desc = "Execute opencode action…" })
+vim.keymap.set("x", "<leader>os", function() return require("opencode").operator("@this ") end, { desc = "Add range to opencode", expr = true })
